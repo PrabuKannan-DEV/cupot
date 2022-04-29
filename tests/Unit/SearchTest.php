3125a92bc93c1d4c760a3c7caff687e9661253fb
Prabu Kannan <coderkatana@gmail.com>
2022-04-29 10:35:32 +0530

    Minor refactor

--- tests/Unit/SearchTest.php
+++ tests/Unit/SearchTest.php
@@ -24,17 +24,7 @@ class SearchTest extends TestCase
         $this->markTestSkipped('This doesn\'t work for the scoring methods, so skipping');
         $current_user = User::factory()->create();
         $user_count = User::count(); //Total count
-        User::factory()->count(5)->create([
-            'dob' => '1998-08-09',
-            'gender' => 'Male',
-            'annual_income' => 11000,
-            'occupation' => 'Private Job',
-            'family_type' => 'Joint Faimly',
-            'manglik' => 'Yes'
-        ]);
-        $this->assertEquals($user_count+5, User::count()); //Asserting total count
-
-        $input_data = [
+        $data_1 = [
             'dob' => '1998-08-09',
             'gender' => 'Male',
             'annual_income' => 11000,
@@ -42,29 +32,26 @@ class SearchTest extends TestCase
             'family_type' => 'Joint Faimly',
             'manglik' => 'Yes'
         ];
-        [$results, $count] = Search::basicSearch($input_data, $current_user->id);
+        User::factory()->count(5)->create($data_1);
+        $this->assertEquals($user_count+5, User::count()); //Asserting total count
+
+        [$results, $count] = Search::basicSearch($data_1, $current_user->id);
         $results_count = $results->count();
         $this->assertEquals(5, $results_count);//Asserting 1st set of search results
 
         $user_count = User::count();
-        User::factory()->count(5)->create([
+        $data_2 = [
             'dob' => '2020-08-09',
             'gender' => 'Female',
             'annual_income' => 21000,
             'occupation' => 'Government Job',
             'family_type' => 'Nuclear Faimly',
             'manglik' => 'Yes'
-        ]);
-        $this->assertEquals($user_count+5, User::count());
-        $input_data = [
-            'dob' => '2020-08-09',
-            'gender' => 'Female',
-            'annual_income' => 21000,
-            'occupation' => 'Government Job',
-            'family_type' => 'Nuclear Faimly',
-            'manglik' => 'Yes',
         ];
-        [$results, $count] = Search::basicSearch($input_data, $current_user->id);
+        User::factory()->count(5)->create($data_2);
+        $this->assertEquals($user_count+5, User::count());
+
+        [$results, $count] = Search::basicSearch($data_2, $current_user->id);
         $results_count = $results->count();
         $this->assertEquals(5, $results_count);//Asserting 2nd set of search results
     }
